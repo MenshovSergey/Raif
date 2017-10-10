fc2fac04a8674c32d4ad6e72c133a424807a42d6
MenshovSergey <menshov-sergej@yandex.ru>
2017-10-10 17:25:45 +0300

    Comparsion classifiers

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import sklearn.preprocessing as preproccesing
-cat_coulmns = ["Event_type", "Insurer_type", "Owner_region", "Owner_type", "Sales_channel", "VEH_aim_use", "VEH_model",
-               "VEH_type_name"]
-drop_columns = ["bad","claim_id"]
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+from sklearn import svm
+from sklearn.dummy import DummyClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
+from sklearn.metrics import accuracy_score
+
+cat_coulmns = ["Event_type", "Insurer_type", "Owner_region", "Owner_type", "Sales_channel", "VEH_aim_use", "VEH_model",
+               "VEH_type_name"]
+drop_columns = ["bad", "claim_id"]
+names = ["RandomForest", "GBM", "AdaBoost", "SVM", "Dummy"]
+classifiers = [RandomForestClassifier(random_state=42), GradientBoostingClassifier(random_state=42),
+               AdaBoostClassifier(random_state=42),
+               svm.SVC(random_state=42), DummyClassifier(random_state=42)]
+
 
 def read_data():
     data = pd.read_csv("data/all.csv", sep=';', decimal=",")
     data = pd.get_dummies(data, columns=cat_coulmns)
     y = data.get("bad")
-    data = data.drop(drop_columns,1)
+    data = data.drop(drop_columns, 1)
     data = data.fillna(method='pad')
     return data, y
+
 
 def normalize_data(data):
     scaler = preproccesing.MinMaxScaler()
@@ -22,19 +34,29 @@ def normalize_data(data):
     return scaler.transform(data)
 
 
-def main():
-    X, Y = read_data()
-    X_norm = normalize_data(X)
-    rf = RandomForestClassifier(random_state=42)
-    X_train, X_test, y_train, y_test = train_test_split(X_norm,Y,random_state=50)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
-    print("Accuracy : "+str(rf.score(X_test, y_test)))
+def show_metrics(y_test, y_pred):
+    print("Accuracy : " + str(accuracy_score(y_test, y_pred)))
     print("...")
     print("Confusion matrix")
     print(confusion_matrix(y_test, y_pred))
     print("...")
-    print("F1 measure : " + str(f1_score(y_test, y_pred, average='macro')))
+    print("F1 measure : " + str(f1_score(y_test, y_pred, average=None)))
+
+
+def compare_classifiers(X, Y, names, classifiers):
+    X_train, X_test, y_train, y_test = train_test_split(X, Y, random_state=50)
+    for name, classifier in zip(names, classifiers):
+        classifier.fit(X_train, y_train)
+        y_pred = classifier.predict(X_test)
+        print(name)
+        show_metrics(y_test, y_pred)
+        print()
+
+
+def main():
+    X, Y = read_data()
+    X_norm = normalize_data(X)
+    compare_classifiers(X_norm, Y, names, classifiers)
 
 
 main()
