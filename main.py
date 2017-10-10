35bc965bce45326b0ffb82077304e90f3f9463aa
MenshovSergey <menshov-sergej@yandex.ru>
2017-10-10 16:26:33 +0300

    Add metrics

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ cat_coulmns = ["Event_type", "Insurer_type", "Owner_region", "Owner_type", "Sale
 drop_columns = ["bad","claim_id"]
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
 
 def read_data():
     data = pd.read_csv("data/all.csv", sep=';', decimal=",")
@@ -23,8 +25,16 @@ def normalize_data(data):
 def main():
     X, Y = read_data()
     X_norm = normalize_data(X)
-    rf = RandomForestClassifier()
-    X_train, X_test, y_train, y_test = train_test_split(X_norm,Y)
+    rf = RandomForestClassifier(random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X_norm,Y,random_state=50)
     rf.fit(X_train, y_train)
-    print(rf.score(X_test, y_test))
+    y_pred = rf.predict(X_test)
+    print("Accuracy : "+str(rf.score(X_test, y_test)))
+    print("...")
+    print("Confusion matrix")
+    print(confusion_matrix(y_test, y_pred))
+    print("...")
+    print("F1 measure : " + str(f1_score(y_test, y_pred, average='macro')))
+
+
 main()
